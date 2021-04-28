2bc8fd0f72a0a58a6754ff9b4d5918041c3ca589
YannickMontes <montes.yannick@gmail.com>
2021-04-28 20:41:22 +0200

    First example

--- routes/products.js
+++ routes/products.js
@@ -1,18 +1,10 @@
 const express = require('express');
 const format = require('../joi_request_format');
 const Product = require('../models/Product');
+const productController = require('../controller/productController');
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    try{
-        const products = await Product.find();
-        res.status(200).json(products);
-    }
-    catch(error)
-    {
-        res.status(500).json({message:error})
-    }
-});
+router.get('/', productController.getAllProducts);
 
 router.get('/:id', async (req, res) => {
     try{
